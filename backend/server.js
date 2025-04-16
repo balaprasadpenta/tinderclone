cce5074410ccebe44151ede5e3ad22b2c3b0184b
Balaprasad Penta <balaprasadpentawar@gmail.com>
2025-04-16 20:49:45 +0530

    7

--- backend/server.js
+++ backend/server.js
@@ -38,10 +38,10 @@ app.use("/messages", messageRoutes);
 app.use("/matches", matchRoutes);
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, '/client/dist')))
+  app.use(express.static(path.join(__dirname, '/frontend/dist')))
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html" ))
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html" ))
   })
 }
 
